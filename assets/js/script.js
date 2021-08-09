fb1c158dec3acc62b8dbb534e24bcac4a3442c1f
aclar <a.clarke0307@gmail.com>
2021-08-09 18:25:55 -0400

    js changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,34 @@
- document.querySelector("form").addEventListener("submit",function(e){
-   e.preventDefault();
-   var searchedCity =document.getElementById("searchCity").value;
-   console.log(searchedCity);
+var button = document.querySelector(".button")
+var inputValue = document.querySelector(".inputValue")
+var nameEl = document.querySelector(".name");
+var temp =document.querySelector(".temp");
+var humidity =document.querySelector(".humidity");
+var wind = document.querySelector(".wind");
+var listCity =document.querySelector(".list-city");
 
-   fetch('http://api.openweathermap.org/data/2.5/weather?q='+searchedCity+'&APPID=f2282ab08d953c2ff58c683d42d09c9a')
-        .then(function(response){
-            return response.json();
-        })
-        .then(function(data){
-            document.getElementById('city-list').innerHTML=" ";
-            data.weather.forEach(function(city){
-                var pEl =document.createElement("p");
-                pEl.textContent= city.id.name;
-                document.getElementById('city-list').appendChild(pEl);
-            })
+
+
+button.addEventListener("click", function(){
+fetch('http://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&APPID=f2282ab08d953c2ff58c683d42d09c9a')
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data)
+        var nameValue = data['name'];
+        var tempValue = data['main']['temp'];
+        var humidityValue = data['main']['humidity'];
+        var windValue = data['wind']['speed'];
         
-        });
 
- });
+        nameEl.innerHTML = nameValue;
+        temp.innerHTML ="Temp:" + " " + tempValue + "Degrees Fahrenheit";
+        humidity.innerHTML = "Humidity:" + " "+ humidityValue;
+        wind.innerHTML = "Wind:" + " "+ windValue + "MPH";
+        
+        listCity.append(nameValue);
+    })
+    .catch(err => alert("No City Found!"))
+
+});
+localStorage.getItem('value');
