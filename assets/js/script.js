a274942db6aa8e3a54e4c22e74fb1e3967a14d03
aclar <a.clarke0307@gmail.com>
2021-08-09 19:08:05 -0400

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,13 +28,8 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&AP
         
     
     })
-   // .catch(err => alert("No City Found!"))
+   .catch(err => alert("No City Found!"))
 
 });
 
-if (location.protocol === 'http:') {
-    url = 'http://api.openweathermap.org/http://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&APPID=f2282ab08d953c2ff58c683d42d09c9adata/2.5/weather?lat=21.1682895&lon=-101.6723306&units=imperial&APPID=ec50a6072ac189dee111acdd3a38ab9f';
-} else {
-   url = 'https://api.openweathermap.org/dathttp://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&APPID=f2282ab08d953c2ff58c683d42d09c9aa/2.5/weather?lat=21.1682http://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&APPID=f2282ab08d953c2ff58c683d42d09c9a895&lon=-101.6723306&units=imperial&APPID=ec50a6072ac189dee111acdd3a38ab9f';
-};
 localStorage.getItem('value');
